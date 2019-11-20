0c2edc86ca5f4be9d3947952c923266537e67b16
Buck Baskin <mobile.wbaskin@gmail.com>
2019-11-20 01:39:02 -0800

    Tooling update

--- check.bash
+++ check.bash
@@ -1,1 +1,8 @@
-clang-check-6.0 -p build/compile_commands.json $(ag --cpp -l -g ".*" src/ test/)
+HEADER_FILES=$(ag --cpp -l -g ".*" include/)
+SOURCE_FILES=$(ag --cpp -l -g ".*" src/)
+TEST_FILES=$(ag --cpp -l -g ".*" test/)
+ALL_FILES=$(echo ${HEADER_FILES} ${SOURCE_FILES} ${TEST_FILES})
+
+clang-check-6.0 -p build/compile_commands.json ${SOURCE_FILES} ${TEST_FILES} &&\
+cppcheck --language=c++ --std=c++14 ${ALL_FILES} &&\
+cpplint ${ALL_FILES}
