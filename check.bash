dd7fc80e984e2bc2050ac895c67935d93842ebc5
Buck Baskin <mobile.wbaskin@gmail.com>
2019-11-23 00:38:44 -0800

    Move to a more boost-like directory structure

--- check.bash
+++ check.bash
@@ -2,7 +2,7 @@
 cat check.bash
 
 HEADER_FILES=$(ag --cpp -l -g ".*" include/)
-SOURCE_FILES=$(ag --cpp -l -g ".*" src/)
+SOURCE_FILES=$(ag --cpp -l -g ".*" src/ example/)
 TEST_FILES=$(ag --cpp -l -g ".*" test/)
 ALL_FILES=$(echo ${HEADER_FILES} ${SOURCE_FILES} ${TEST_FILES})
 
