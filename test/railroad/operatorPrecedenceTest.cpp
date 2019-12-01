0e7bf97bc1c3fbdc025e62bdda93959e6a68d940
Buck Baskin <mobile.wbaskin@gmail.com>
2019-11-30 21:20:01 -0800

    Demonstrate >>= on full funcs

--- test/railroad/operatorPrecedenceTest.cpp
+++ test/railroad/operatorPrecedenceTest.cpp
@@ -12,12 +12,13 @@
 
 namespace {
 
-using ::railroad::binds;
+using ::railroad::bindr;
+using ::railroad::DefaultFailure;
 using ::railroad::Result;
 using ::railroad::Success;
 using ::railroad::result::PartialSuccessResult;
 
-TEST_CASE(">>= works on full func", "[operatorPrecedence]") {
+TEST_CASE(">>= works on full func simple", "[operatorPrecedence]") {
   std::function<Result<int, int>(Result<int, int>)> adder =
       [](Result<int, int> input) -> Result<int, int> {
     if (input.hasFailure()) {
@@ -39,24 +40,38 @@ TEST_CASE(">>= works on full func", "[operatorPrecedence]") {
   }));
 }
 
-/*
-TEST_CASE(">>= works on partialS->S func", "[operatorPrecedence]") {
-  std::function<PartialSuccessResult<int>(PartialSuccessResult<int>)> adder =
-      [](PartialSuccessResult<int> i) {
-        return PartialSuccessResult<int>{i.unpack() + 1};
-      };
-  std::function feed = ::railroad::helpers::feedSuccess<int>;
-  std::function terminate = ::railroad::helpers::terminateSuccess<int>;
+TEST_CASE(">>= works on full func", "[operatorPrecedence]") {
+  std::function<Result<int, int>(Result<int, int>)> adder =
+      [](Result<int, int> input) -> Result<int, int> {
+    if (input.hasFailure()) {
+      return input;
+    } else {
+      return Success<int, int>(input.getSuccess() + 1);
+    }
+  };
 
-  REQUIRE(rc::check([feed, adder, terminate](int checkThis) {
-    Result<int, int> explicitChain = (feed >> bind(adder))(checkThis);
-    Result<int, int> implicitChain = (feed >>= adder)(checkThis);
+  REQUIRE(rc::check([adder](int checkThis) {
+    Result<int, int> explicitChain =
+        (adder >> bindr(adder) >> bindr(adder))(Success<int, int>(checkThis));
+
+    Result<int, int> preMixedChain =
+        (adder >>= adder >> bindr(adder))(Success<int, int>(checkThis));
+
+    Result<int, int> postMixedChain =
+        (adder >> bindr(adder) >>= adder)(Success<int, int>(checkThis));
+
+    Result<int, int> implicitChain =
+        (adder >>= adder >>= adder)(Success<int, int>(checkThis));
 
     REQUIRE(explicitChain.hasSuccess());
+    REQUIRE(preMixedChain.hasSuccess());
+    REQUIRE(postMixedChain.hasSuccess());
     REQUIRE(implicitChain.hasSuccess());
-    REQUIRE(explicitChain.getSuccess() == implicitChain.getSuccess());
+
+    REQUIRE(implicitChain.getSuccess() == explicitChain.getSuccess());
+    REQUIRE(implicitChain.getSuccess() == preMixedChain.getSuccess());
+    REQUIRE(implicitChain.getSuccess() == postMixedChain.getSuccess());
   }));
 }
-*/
 
 }  // namespace
